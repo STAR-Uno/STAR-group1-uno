103e563317f0d0676e3ecd25c086bd0a64396c8f
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2023-03-24 13:46:10 +0100

    make constant variables uppercase

--- src/main/java/ui/view/CardView.java
+++ src/main/java/ui/view/CardView.java
@@ -14,11 +14,11 @@ public class CardView extends JPanel {
     private final Card card;
     private final String value;
 
-    private static final int cardWidth = 100;
-    private static final int cardHeight = 150;
-    private static final int margin = 5;
+    private static final int CARD_WIDTH = 100;
+    private static final int CARD_HEIGHT = 150;
+    private static final int MARGIN = 5;
 
-    private final Dimension dimension = new Dimension(cardWidth, cardHeight);
+    private final Dimension dimension = new Dimension(CARD_WIDTH, CARD_HEIGHT);
 
     private final transient Border defaultBorder = BorderFactory.createEtchedBorder(WHEN_FOCUSED, Color.white, Color.gray);
     private final transient Border focusedBorder = BorderFactory.createEtchedBorder(WHEN_FOCUSED, Color.black, Color.gray);
@@ -82,38 +82,38 @@ public class CardView extends JPanel {
 
     private void fillBackground(Graphics2D g2, Color cardColor) {
         g2.setColor(Color.WHITE);
-        g2.fillRect(0, 0, cardWidth, cardHeight);
+        g2.fillRect(0, 0, CARD_WIDTH, CARD_HEIGHT);
 
         g2.setColor(cardColor);
-        g2.fillRect(margin, margin, cardWidth - 2 * margin, cardHeight - 2 * margin);
+        g2.fillRect(MARGIN, MARGIN, CARD_WIDTH - 2 * MARGIN, CARD_HEIGHT - 2 * MARGIN);
     }
 
     private void drawWhiteOvalInCenter(Graphics2D g2) {
         var transformer = g2.getTransform();
         g2.setColor(Color.white);
-        g2.rotate(45, (double) cardWidth * 3 / 4, (double) cardHeight * 3 / 4);
-        g2.fillOval(0, cardHeight / 4, cardWidth * 3 / 5, cardHeight);
+        g2.rotate(45, (double) CARD_WIDTH * 3 / 4, (double) CARD_HEIGHT * 3 / 4);
+        g2.fillOval(0, CARD_HEIGHT / 4, CARD_WIDTH * 3 / 5, CARD_HEIGHT);
 
         g2.setTransform(transformer);
     }
 
     private void drawValueInCenter(Graphics2D g2, Color cardColor) {
-        var defaultFont = new Font(StyleUtil.DEFAULT_FONT, Font.BOLD, cardWidth / 2 + 5);
+        var defaultFont = new Font(StyleUtil.DEFAULT_FONT, Font.BOLD, CARD_WIDTH / 2 + 5);
         var fontMetrics = this.getFontMetrics(defaultFont);
         int stringWidth = fontMetrics.stringWidth(value) / 2;
         int fontHeight = defaultFont.getSize() / 3;
 
         g2.setColor(cardColor);
         g2.setFont(defaultFont);
-        g2.drawString(value, cardWidth / 2 - stringWidth, cardHeight / 2 + fontHeight);
+        g2.drawString(value, CARD_WIDTH / 2 - stringWidth, CARD_HEIGHT / 2 + fontHeight);
     }
 
     private void drawValueInCorner(Graphics2D g2) {
-        var defaultFont = new Font(StyleUtil.DEFAULT_FONT, Font.ITALIC, cardWidth / 5);
+        var defaultFont = new Font(StyleUtil.DEFAULT_FONT, Font.ITALIC, CARD_WIDTH / 5);
 
         g2.setColor(Color.white);
         g2.setFont(defaultFont);
-        g2.drawString(value, margin, 5 * margin);
+        g2.drawString(value, MARGIN, 5 * MARGIN);
     }
 
     private void showHoverEffect(){
